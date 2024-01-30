5cc49a4151f0f608a5d59e2883d08f3bb84c0a72
raphouqld <raphael@quillaud.org>
2024-01-30 20:21:46 +0100

    untouched

---
+++ seq 5 WEB/TP4_WEB_formulaire/traitement_formulaire_GET.php
@@ -0,0 +1,10 @@
+<html lang="fr">
+  <head>
+    <meta charset="UTF-8" />
+    <title>Formulaire Test</title>
+  </head>
+  <body>
+	  <h1>Passage de paramètres par la méthode GET</h1>
+	  <p>Bonjour  , bienvenue sur mon site Web.</p>
+  </body>
+</html>

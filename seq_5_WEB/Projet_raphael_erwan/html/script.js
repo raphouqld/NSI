06a7cfa421ce88c91018811c2ca6f04f7b1eda94
raphouqld <raphael@quillaud.org>
2024-02-08 20:23:37 +0100

    Répertoire téléphonique fonctionnel

--- seq_5_WEB/Projet_raphael_erwan/html/script.js
+++ seq_5_WEB/Projet_raphael_erwan/html/script.js
@@ -1,6 +1,22 @@
-function ajouter(){
-    //récuperer les élements
-    var nom = document.getElementById("nom").value;
-    var numero = document.getElementById("numero").value;
-    print(nom,numero);
+let repertoire = ["Quillaud","0769125614","Detambel","0664934770"]
+
+function ajouterNum(){
+    // Récuperer les élements
+    var quelNom = prompt("Quel est votre nom ?")
+    var quelNumero = prompt("Quel est votre Numéro ?");
+
+    // Ajouter au tableau
+    repertoire.push(quelNom,quelNumero); 
+}
+
+function rechercherNum(){
+    // Récuperer
+    var nom = prompt("Entrer un nom de famille");
+    var posNom = repertoire.indexOf(nom);
+    var posNumero = posNom+1
+    if (posNom != -1){
+        document.getElementById("outputRechercher").innerHTML = repertoire[posNumero];
+    }else{
+        document.getElementById("outputRechercher").innerHTML = "Le nom que vous avez renseigné n'est pas dans le répertoire."
+    }
 }

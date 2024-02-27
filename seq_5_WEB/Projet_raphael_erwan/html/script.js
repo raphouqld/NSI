07ba0dfbe91e34a154e59363b8ee5c3dcf6c74d4
raphouqld <raphael@quillaud.org>
2024-02-27 08:49:51 +0100

    dark & light modes

--- seq_5_WEB/Projet_raphael_erwan/html/script.js
+++ seq_5_WEB/Projet_raphael_erwan/html/script.js
@@ -20,3 +20,15 @@ function rechercherNum(){
         document.getElementById("outputRechercher").innerHTML = "Le nom que vous avez renseigné n'est pas dans le répertoire."
     }
 }
+
+function changerTheme(){
+    var lienCSS = document.getElementById('lienCSS'); // Obtient l'élément link qui lie le fichier CSS
+    var nomFichierCSS = lienCSS.getAttribute('href'); // Obtient le nom du fichier CSS actuellement lié
+
+    // Détermine quel thème est actuellement appliqué
+    if (nomFichierCSS === 'style_light.css') {
+        lienCSS.setAttribute('href', 'style_dark.css'); // Change vers le thème sombre
+    } else {
+        lienCSS.setAttribute('href', 'style_light.css'); // Change vers le thème clair
+    }
+}

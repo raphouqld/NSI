de07ba035004bb147b1550ffcd0ccb08800df4c1
raphouqld <raphael@quillaud.org>
2024-02-27 09:47:25 +0100

    ajouter un "numéro" -> ajouter un "contact" js : added all themes, changing names in lowercase

--- seq_5_WEB/Projet_raphael_erwan/html/script.js
+++ seq_5_WEB/Projet_raphael_erwan/html/script.js
@@ -1,19 +1,23 @@
-let repertoire = ["Raphaël","0769125614","Erwan","0664934770"]
+let repertoire = ["raphael","0769125614","erwan","0664934770"]
 
 function ajouterNum(){
     // Récuperer les élements
     var quelNom = prompt("Quel est votre nom ?")
+    quelNom.toLowerCase(); // Mettre en minuscule
     var quelNumero = prompt("Quel est votre Numéro ?");
 
-    // Ajouter au tableau
+    // Ajouter au repertoire
     repertoire.push(quelNom,quelNumero); 
 }
 
 function rechercherNum(){
     // Récuperer
     var nom = prompt("Entrer un nom");
+    nom.toLowerCase(); // Mettre en minuscule
     var posNom = repertoire.indexOf(nom);
     var posNumero = posNom+1
+
+    // Chercher le numero dans le repertoire
     if (posNom != -1){
         document.getElementById("outputRechercher").innerHTML = "Le numéro de "+nom+" est "+ repertoire[posNumero];
     }else{
@@ -22,13 +26,19 @@ function rechercherNum(){
 }
 
 function changerTheme(){
-    var lienCSS = document.getElementById('lienCSS'); // Obtient l'élément link qui lie le fichier CSS
-    var nomFichierCSS = lienCSS.getAttribute('href'); // Obtient le nom du fichier CSS actuellement lié
+    var lienCSS = document.getElementById('lienCSS');
+    var nomFichierCSS = lienCSS.getAttribute('href');
 
-    // Détermine quel thème est actuellement appliqué
     if (nomFichierCSS === 'style_light.css') {
         lienCSS.setAttribute('href', 'style_dark.css'); // Change vers le thème sombre
-    } else {
-        lienCSS.setAttribute('href', 'style_light.css'); // Change vers le thème clair
+    }
+    else if (nomFichierCSS === 'style_dark.css') {
+        lienCSS.setAttribute('href', 'style_matrix.css'); // Change vers le thème matrix
+    }
+    else if (nomFichierCSS === 'style_matrix.css') {
+        lienCSS.setAttribute('href','style_space.css'); // Change vers le thème espace
+    }
+    else if (nomFichierCSS === 'style_space.css') {
+        lienCSS.setAttribute('href','style_light.css'); // Change vers le thème clair
     }
 }

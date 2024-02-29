388e06889e98508b783e4cbe0618d7be6e847046
raphouqld <raphael@quillaud.org>
2024-02-29 16:09:13 +0100

    repertoire python

--- seq_5_WEB/Projet_raphael_erwan/repertoire_telephonique.py
+++ seq_5_WEB/Projet_raphael_erwan/repertoire_telephonique.py
@@ -1,23 +1,35 @@
+repertoire = ["raphael","07 69 12 56 14","erwan","07 46 58 39 29"]
+
 def afficherMenu() :
+    print("\n\n***** MENU *****")
     print("\nChoisissez une option")
     print("1. Ajouter un numéro")
     print("2. Rechercher un numéro")
     print("3. Quitter")
 
 def ajouterNumero() :
-    print("coucou")
+    nom = input("Nom du contact : ")
+    numero = input("Numéro de téléphone : ")
+    repertoire.append(nom)
+    repertoire.append(numero)
 
 def rechercherNumero() :
-    print("coucou")
+    nom = input("Nom : ")
+    if nom in repertoire :
+        posnom = repertoire.index(nom)
+        print("\nLe numéro de téléphone de ",nom," est : ",repertoire[posnom + 1])
+    else :
+        print("\nCe nom n'est pas dans le répertoire.")
 
 while True :
     afficherMenu()
     choix = input("Choix : ")
-    if choix == 1 :
+    if choix == "1" :
         ajouterNumero()
-    elif choix == 2 :
+    elif choix == "2" :
         rechercherNumero()
-    elif choix == 3 :
+    elif choix == "3" :
+        print( "\nAu revoir !\n")
         break
     else :
-        print("Merci d'entrer un nombre valide (1/2/3)")
+        print("\nMerci d'entrer un nombre valide (1/2/3)")
